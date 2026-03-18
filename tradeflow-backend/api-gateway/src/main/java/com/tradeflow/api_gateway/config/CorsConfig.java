f21434b408628bab7a1ff0acc11de9da6ed82871
Hardik Bhanot <76391935+Hardikbhanot@users.noreply.github.com>
2026-03-18 10:03:42 +0000

    Added CORS config to link frontend to backend

---
+++ tradeflow-backend/api-gateway/src/main/java/com/tradeflow/api_gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.tradeflow.api_gate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        // This allows your Vercel frontend to talk to this Bangalore API
+        config.addAllowedOrigin("https://tradeflow.hbhanot.tech"); 
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
